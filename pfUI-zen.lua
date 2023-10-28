798cbe4ad481d59c72b943fafbfde09ee9bb9f46
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-10-28 22:08:15 +0200

    feat (pfUI-zen.lua): scaffold

---
+++ pfUI-zen.lua
@@ -0,0 +1,13 @@
+﻿---@diagnostic disable: undefined-global
+pfUI:RegisterModule("Zen", "vanilla:tbc", function ()
+  local _ = {
+    T = T,
+    pfUI = pfUI,
+  } ---@diagnostic enable: undefined-global
+
+  _.pfUI.gui.dropdowns.Zen_green_items_roll = {
+    "need:" .. _.T["Need"],
+    "greed:" .. _.T["Greed"],
+    "pass:" .. _.T["Pass"]
+  }
+end)
