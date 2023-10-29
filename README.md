2d567d35008c70441eb8c5631c67c527a4f46356
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-10-29 21:17:21 +0100

    doc (Readme.md): trivial tweaks

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# pfUI-zen
+# <span style="color: cyan">pf</span><span style="color: orange">UI</span></span> <span style="color: aquamarine; font-size: 44px;">Zen</span>
 
-Quality-of-life addon for vanilla-warcraft (1.12) providing in-game utilities, enhancements, reasonable defaults and automations boosting your in-game Zen.
+Quality-of-life addon for vanilla-warcraft (1.12) providing in-game utilities, enhancements, reasonable defaults and automations boosting your in-game Zen ;)
 
-# Development Environment Setup
+## Development Environment Setup
 
-    Note: You're better off using the the official addon package (WIP - coming soon) for an in-game optimized version of this addon.
+    Note: You're better off using the the official addon-package (WIP - coming soon) for an optimum
+    gaming experience.
 
 ```bash
 git clone git@github.com:dsidirop/pfUI-zen.git   --recurse-submodules
--- pfUI-zen.lua
+++ pfUI-zen.lua
@@ -1,16 +1,12 @@
-﻿pfUI:RegisterModule("Zen", "vanilla:tbc", function()
+﻿pfUI:RegisterModule("Zen", "vanilla:tbc", function() -- inspired by pfUI-eliteOverlay.lua
     local __ = {
+        C = C,
         T = T,
         F = F,
+        U = pfUI.gui.UpdaterFunctions,
         linq = linq,
         pfUI = pfUI,
         GetAddOnInfo = GetAddOnInfo,
-    }
-
-    __.pfUI.gui.dropdowns.Zen_green_items_roll = {
-        "need:" .. __.T["Need"],
-        "greed:" .. __.T["Greed"],
-        "pass:" .. __.T["Pass"]
     }
 
     local addonPath = __.linq({ "", "-dev", "-master", "-tbc", "-wotlk" }) -- @formatter:off   detect current addon path
@@ -21,7 +17,35 @@
             :where(function (x) return x.enabled == 1 end)
             :select(function (x) return x.path end)
             :first() -- @formatter:on
-    
-    print(addonPath)
+
+    if (not __.pfUI.gui.CreateGUIEntry) then
+        -- new pfUI
+        print("[PFUIZ.IM000] pfUI-zen needs a recent version of pfUI (2023+) to work as intended - please update pfUI and try again!")
+        return
+    end
+
+    __.pfUI.gui.dropdowns.Zen_green_items_loot_gambling_mode = {
+        "gamble_need:" .. __.T["Gamble Need"],
+        "gamble_greed:" .. __.T["Gamble Greed"],
+        "pass:" .. __.T["Pass"],
+        "disabled:" .. __.T["Let me handle it!"],
+    }
+
+    __.pfUI.gui.CreateGUIEntry(
+            __.T["Thirdparty"],
+            __.T["Zen"],
+            function()
+                __.pfUI.gui.CreateConfig(
+                        __.U["target"], -- nil causes a ui-reload whenever the setting changes
+                        __.T["On Green items always ..."],
+                        __.C.Zen,
+                        "green_items_loot_gambling_mode",
+                        "dropdown",
+                        __.pfUI.gui.dropdowns.Zen_green_items_loot_gambling_mode
+                )
+            end
+    )
+
+    pfUI:UpdateConfig("Zen", nil, "green_items_loot_gambling_mode", "gamble_greed")
 
 end)
