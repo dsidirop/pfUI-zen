d9c58b9de4dcbe6d76b0c25b3c52a77bc86df3a3
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-10-29 05:06:27 +0100

    feat (pfUI-zen.lua): detect current addon path upon activation

--- pfUI-zen.lua
+++ pfUI-zen.lua
@@ -1,13 +1,23 @@
-﻿---@diagnostic disable: undefined-global
-pfUI:RegisterModule("Zen", "vanilla:tbc", function ()
-  local _ = {
-    T = T,
-    pfUI = pfUI,
-  } ---@diagnostic enable: undefined-global
+﻿pfUI:RegisterModule("Zen", "vanilla:tbc", function()
+    local _ = {
+        T = T,
+        F = F,
+        linq = linq,
+        pfUI = pfUI,
+        GetAddOnInfo = GetAddOnInfo,
+    }
 
-  _.pfUI.gui.dropdowns.Zen_green_items_roll = {
-    "need:" .. _.T["Need"],
-    "greed:" .. _.T["Greed"],
-    "pass:" .. _.T["Pass"]
-  }
+    _.pfUI.gui.dropdowns.Zen_green_items_roll = {
+        "need:" .. _.T["Need"],
+        "greed:" .. _.T["Greed"],
+        "pass:" .. _.T["Pass"]
+    }
+
+    local addonpath = _.linq({ "", "-dev", "-master", "-tbc", "-wotlk" }) -- @formatter:off   detect current addon path
+            :select(function (postfix) return _.GetAddOnInfo("pfUI-zen"..postfix) end)
+            :where(function (_, title) return type(title) == "string" end)
+            :first() -- @formatter:on
+    
+    print(addonpath)
+
 end)
