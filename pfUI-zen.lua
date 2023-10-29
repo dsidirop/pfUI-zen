a045df053f3225b5bcec0d4b6539cd930091c90c
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-10-29 22:15:14 +0100

    feat (pfUI-zen.lua): color code menu options for the green_items_loot_autogambling_mode

--- pfUI-zen.lua
+++ pfUI-zen.lua
@@ -24,28 +24,28 @@
         return
     end
 
-    __.pfUI.gui.dropdowns.Zen_green_items_loot_gambling_mode = {
-        "gamble_need:" .. __.T["Gamble Need"],
-        "gamble_greed:" .. __.T["Gamble Greed"],
-        "pass:" .. __.T["Pass"],
-        "disabled:" .. __.T["Let me handle it!"],
+    __.pfUI.gui.dropdowns.Zen_green_items_loot_autogambling_mode = {
+        "roll_need:" .. __.T["Roll '|cFFFF4500Need|r'"],
+        "roll_greed:" .. __.T["Roll '|cFFFFD700Greed|r'"],
+        "pass:" .. __.T["Just '|cff888888Pass|r'"],
+        "disabled:" .. __.T["Let me handle it myself"],
     }
 
     __.pfUI.gui.CreateGUIEntry(
             __.T["Thirdparty"],
-            __.T["Zen"],
+            __.T["|cFF7FFFD4Zen"],
             function()
                 __.pfUI.gui.CreateConfig(
                         __.U["target"], -- nil causes a ui-reload whenever the setting changes
-                        __.T["On Green items always ..."],
+                        __.T["When looting |cFF228B22Green|r items always ..."],
                         __.C.Zen,
-                        "green_items_loot_gambling_mode",
+                        "green_items_loot_autogambling_mode",
                         "dropdown",
-                        __.pfUI.gui.dropdowns.Zen_green_items_loot_gambling_mode
+                        __.pfUI.gui.dropdowns.Zen_green_items_loot_autogambling_mode
                 )
             end
     )
 
-    pfUI:UpdateConfig("Zen", nil, "green_items_loot_gambling_mode", "gamble_greed")
+    pfUI:UpdateConfig("Zen", nil, "green_items_loot_autogambling_mode", "roll_greed")
 
 end)
