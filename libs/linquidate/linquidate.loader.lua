af2fbe92a628b555f91f13e307ed84f36990e72f
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-11-05 19:52:48 +0100

    clean (linquidate.loader.lua)

--- libs/linquidate/linquidate.loader.lua
+++ libs/linquidate/linquidate.loader.lua
@@ -1,21 +1,23 @@
 local _G = _G or getfenv(0)
+
 local funcs = _G.Linquidate_Loader_funcs
 if not funcs then
-	_G.Linquidate_Loader = nil
-	return
+    _G.Linquidate_Loader = nil
+    return
 end
 
-local assert = _G.assert(_G.assert)
+local assert = _G.assert
 
 local type = assert(_G.type)
+local table_getn = assert(_G.table.getn)
 local table_remove = assert(_G.table.remove)
 
 local Linquidate = _G.LibStub("Linquidate")
-while table.getn(funcs) > 0 do
-	local func = table_remove(funcs, 1)
-	if type(func) == "function" then
-		func(Linquidate)
-	end
+while table_getn(funcs) > 0 do
+    local func = table_remove(funcs, 1)
+    if type(func) == "function" then
+        func(Linquidate)
+    end
 end
 
 _G.Linquidate_Loader_funcs = nil
