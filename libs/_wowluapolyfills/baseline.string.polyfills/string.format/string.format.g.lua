8d0addadb39d76834dcdfa73617d48f210b9728a
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-11-11 23:15:20 +0100

    feat (_wowluapolyfills.lua): the polyfills now can used standalone even if the _G symbol is not declared

--- libs/_wowluapolyfills/baseline.string.polyfills/string.format/string.format.g.lua
+++ libs/_wowluapolyfills/baseline.string.polyfills/string.format/string.format.g.lua
@@ -1,4 +1,4 @@
-local _g = assert(_G)
+local _g = assert(_G or assert(getfenv)(0))
 local _gString = assert(_g.string)
 
 _gString.format = _gString.format or assert(string.format)
--- libs/_wowluapolyfills/baseline.string.polyfills/string.match/string.match.g.lua
+++ libs/_wowluapolyfills/baseline.string.polyfills/string.match/string.match.g.lua
@@ -1,4 +1,4 @@
-local _g = assert(_G)
+local _g = assert(_G or assert(getfenv)(0))
 local _gString = assert(_g.string)
 
 _gString.match = _gString.match or assert(string.match)
