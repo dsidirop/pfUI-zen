adcfcb327d9fa08e0c10dabd797df6ea31241de4
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-11-12 00:29:15 +0100

    clean (pfUI-zen.lua): we now explicitly enforce the pfUI global environment to be on the safe side as much as possible

--- pfUI-zen.lua
+++ pfUI-zen.lua
@@ -1,26 +1,24 @@
 -- todo   add artwork at the top of readme.md and inside the configuration page of the addon as a faint watermark
 
-function pfUIZenMain()
-    -- inspired by pfUI-eliteOverlay.lua
-    pfUI:RegisterModule("Zen", "vanilla:tbc", function()
+-- inspired by pfUI-eliteOverlay.lua
+local function Main(_pfUI)
+    _pfUI:RegisterModule("Zen", "vanilla:tbc", function()
+        setfenv(1, _pfUI:GetEnvironment()) -- order        
+        local _g = assert(_G) --              order    dont resort to getfenv(0)   this ought to succeed considering that we called setfenv right above 
 
-        local __ = {
-            pfUI = assert(_G.pfUI),
-            pfuiEnv = assert(_G.pfUI.env),
-            pfuiGui = assert(_G.pfUI.gui),
+        local _c = assert(_g.pfUI.env.C) -- pfUI config
+        local _t = assert(_g.pfUI.env.T) -- pfUI translations
+        local _pfuiGui = assert(_g.pfUI.gui)
 
-            C = assert(_G.pfUI.env.C), -- pfUI config
-            T = assert(_G.pfUI.env.T), -- pfUI translations
+        local _getAddOnInfo = assert(_g.GetAddOnInfo) -- wow api   todo  put this in a custom class called Zen.AddonsHelpers or something
+        
+        local _rollOnLoot = assert(_g.RollOnLoot) -- wow api   todo  put this in a custom class called Zen.LootHelpers or something        
+        local _getItemQualityColor = assert(_g.GetItemQualityColor)
+        local _getLootRollItemLink = assert(_g.GetLootRollItemLink)
+        local _getLootRollItemInfo = assert(_g.GetLootRollItemInfo)
 
-            RollOnLoot = assert(_G.RollOnLoot),
-            Enumerable = assert(_G.Enumerable),
-            GetAddOnInfo = assert(_G.GetAddOnInfo),
-            GetItemQualityColor = assert(_G.GetItemQualityColor),
-            GetLootRollItemLink = assert(_G.GetLootRollItemLink),
-            GetLootRollItemInfo = assert(_G.GetLootRollItemInfo),
-
-            ZenSettingsPfuiForm = assert(_G.ZenSettingsPfuiForm),
-        }
+        local _enumerable = assert(_g.Enumerable) -- addon specific
+        local _zenSettingsPfuiForm = assert(_g.ZenSettingsPfuiForm)
 
         local addon = {
             ownName = "Zen",
@@ -33,10 +31,10 @@ function pfUIZenMain()
             fullNameColoredForErrors = "|cff33ffccpf|r|cffffffffUI|r|cffaaaaaa [|r|cFF7FFFD4Zen|r|cffaaaaaa]|r|cffff5555"
         }
 
-        local addonPath = __.Enumerable -- @formatter:off   detect current addon path
+        local addonPath = _enumerable -- @formatter:off   detect current addon path
                             .FromList({ "", "-dev", "-master", "-tbc", "-wotlk" })
                             :Select(function (postfix)
-                                local name, _, _, enabled = __.GetAddOnInfo(addon.folderName .. postfix)
+                                local name, _, _, enabled = _getAddOnInfo(addon.folderName .. postfix)
                                 return { path = name, enabled = enabled or 0 }
                             end)
                             :Where(function (x) return x.enabled == 1 end)
@@ -48,7 +46,7 @@ function pfUIZenMain()
             return
         end
 
-        if (not __.pfuiGui.CreateGUIEntry) then
+        if (not _pfuiGui.CreateGUIEntry) then
             error(string.format("[PFUIZ.IM010] %s : The addon needs a recent version of pfUI (2023+) to work as intended - please update pfUI and try again!", addon.fullNameColoredForErrors))
             return
         end
@@ -61,10 +59,10 @@ function pfUIZenMain()
                     keyname = "v1.greenies_loot_autogambling.v1.mode",
                     default = "roll_greed",
                     options = {
-                        "roll_need:" .. __.T["Roll '|cFFFF4500Need|r'"],
-                        "roll_greed:" .. __.T["Roll '|cFFFFD700Greed|r'"],
-                        "pass:" .. __.T["Just '|cff888888Pass|r'"],
-                        "let_user_choose:" .. __.T["Let me handle it myself"],
+                        "roll_need:" .. _t["Roll '|cFFFF4500Need|r'"],
+                        "roll_greed:" .. _t["Roll '|cFFFFD700Greed|r'"],
+                        "pass:" .. _t["Just '|cff888888Pass|r'"],
+                        "let_user_choose:" .. _t["Let me handle it myself"],
                     },
                 },
 
@@ -72,71 +70,71 @@ function pfUIZenMain()
                     keyname = "v1.greenies_loot_autogambling.v1.keybind",
                     default = "automatic",
                     options = {
-                        "automatic:" .. __.T["|cff888888(Automatic)|r"],
-                        "alt:" .. __.T["Alt"],
-                        "ctrl:" .. __.T["Ctrl"],
-                        "shift:" .. __.T["Shift"],
-                        "ctrl_alt:" .. __.T["Ctrl + Alt"],
-                        "ctrl_shift:" .. __.T["Ctrl + Shift"],
-                        "alt_shift:" .. __.T["Alt + Shift"],
-                        "ctrl_alt_shift:" .. __.T["Ctrl + Alt + Shift"],
+                        "automatic:" .. _t["|cff888888(Automatic)|r"],
+                        "alt:" .. _t["Alt"],
+                        "ctrl:" .. _t["Ctrl"],
+                        "shift:" .. _t["Shift"],
+                        "ctrl_alt:" .. _t["Ctrl + Alt"],
+                        "ctrl_shift:" .. _t["Ctrl + Shift"],
+                        "alt_shift:" .. _t["Alt + Shift"],
+                        "ctrl_alt_shift:" .. _t["Ctrl + Alt + Shift"],
                     },
                 },
             }
         }
 
         --if true then
-        --    __.C.Zen = nil  -- this resets the entire settings tree for this addon
-        --    __.C.Zen2 = nil
-        --    __.C.Zen3 = nil
+        --    _C.Zen = nil  -- this resets the entire settings tree for this addon
+        --    _C.Zen2 = nil
+        --    _C.Zen3 = nil
         --    return
         --end
 
-        -- set default values for the first time we load the addon    this also creates __.C[addon.ownName]={} if it doesnt already exist 
-        __.pfUI:UpdateConfig(addon.ownName, nil, settingsSpecsV1.greenies_loot_autogambling.mode.keyname, settingsSpecsV1.greenies_loot_autogambling.mode.default)
-        __.pfUI:UpdateConfig(addon.ownName, nil, settingsSpecsV1.greenies_loot_autogambling.roll_on_keybind.keyname, settingsSpecsV1.greenies_loot_autogambling.roll_on_keybind.default)
-        
-        __.pfuiGui.CreateGUIEntry(
-                __.T["Thirdparty"],
-                __.T[addon.ownNameColored],
+        -- set default values for the first time we load the addon    this also creates _C[addon.ownName]={} if it doesnt already exist 
+        _pfUI:UpdateConfig(addon.ownName, nil, settingsSpecsV1.greenies_loot_autogambling.mode.keyname, settingsSpecsV1.greenies_loot_autogambling.mode.default)
+        _pfUI:UpdateConfig(addon.ownName, nil, settingsSpecsV1.greenies_loot_autogambling.roll_on_keybind.keyname, settingsSpecsV1.greenies_loot_autogambling.roll_on_keybind.default)
+
+        _pfuiGui.CreateGUIEntry(
+                _t["Thirdparty"],
+                _t[addon.ownNameColored],
                 function()
                     -- this only gets called during a user session the very first time that the user explicitly
                     -- navigates to the "thirtparty" section and clicks on the "zen" tab   otherwise it never gets called
 
-                    __.ZenSettingsPfuiForm:New(
-                            __.T,
-                            __.pfuiGui,
-                            __.C[addon.ownName], -- addonRawPfuiSettings
+                    _zenSettingsPfuiForm:New(
+                            _t,
+                            _pfuiGui,
+                            _c[addon.ownName], -- addonRawPfuiSettings
                             settingsSpecsV1
-                    ) :InitializeControls()
+                    )                   :Initialize()
                 end
         )
 
         local QUALITY_GREEN = 2
-        local _, _, _, greeniesQualityHex = __.GetItemQualityColor(QUALITY_GREEN)
+        local _, _, _, greeniesQualityHex = _getItemQualityColor(QUALITY_GREEN)
 
-        local _base_pfuiRoll_UpdateLootRoll = __.pfUI.roll.UpdateLootRoll
-        function __.pfUI.roll:UpdateLootRoll(i)
+        local _base_pfuiRoll_UpdateLootRoll = _pfUI.roll.UpdateLootRoll
+        function _pfUI.roll:UpdateLootRoll(i)
             -- override pfUI's UpdateLootRoll
             _base_pfuiRoll_UpdateLootRoll(i)
 
-            local rollMode = TranslateAutogamblingModeSettingToLuaRollMode(__.C[addon.ownName][settingsSpecsV1.greenies_loot_autogambling.mode])
+            local rollMode = TranslateAutogamblingModeSettingToLuaRollMode(_c[addon.ownName][settingsSpecsV1.greenies_loot_autogambling.mode])
             if not rollMode then
                 return -- let the user choose
             end
 
-            local frame = __.pfUI.roll.frames[i]
+            local frame = _pfUI.roll.frames[i]
             if not frame or not frame.rollID or not frame:IsShown() then
                 -- shouldnt happen but just in case
                 return
             end
 
-            local _, _, _, quality = __.GetLootRollItemInfo(frame.rollID) -- todo   this could be optimized if we convince pfui to store the loot properties in the frame
+            local _, _, _, quality = _getLootRollItemInfo(frame.rollID) -- todo   this could be optimized if we convince pfui to store the loot properties in the frame
             if quality == QUALITY_GREEN and frame:IsVisible() then
                 -- todo   get keybind activation into account here
-                __.RollOnLoot(frame.rollID, rollMode) -- todo   ensure that pfUI reacts accordingly to this by hiding the green item roll frame
+                _rollOnLoot(frame.rollID, rollMode) -- todo   ensure that pfUI reacts accordingly to this by hiding the green item roll frame
 
-                DEFAULT_CHAT_FRAME:AddMessage(addon.fullNameColored .. " " .. greeniesQualityHex .. rollMode .. "|cffffffff Roll " .. __.GetLootRollItemLink(frame.rollID))
+                DEFAULT_CHAT_FRAME:AddMessage(addon.fullNameColored .. " " .. greeniesQualityHex .. rollMode .. "|cffffffff Roll " .. _getLootRollItemLink(frame.rollID))
             end
         end
 
@@ -161,4 +159,4 @@ function pfUIZenMain()
     end)
 end
 
-pfUIZenMain()
+Main(assert(pfUI))
