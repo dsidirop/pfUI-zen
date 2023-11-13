d348ea3096af6ed7774b2572e76e926e1df4a058
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-11-13 01:07:56 +0100

    feat (namespacer.external.libs.registrar.lua): import libstub and mta-linq enumerables in standardized namespaces

---
+++ module/namespacer.external.libs.registrar.lua
@@ -0,0 +1,12 @@
+﻿local _g = assert(_G or getfenv(0))
+local _setfenv = assert(_g.setfenv)
+local _namespacer_bind = assert(_g.pavilion_pfui_zen_class_namespacer__bind)
+
+local _mta_lualinq_enumerable = assert(_g.Enumerable)
+local _libstub_service_locator = assert(_g.LibStub)
+
+_g = nil
+_setfenv(1, {})
+
+_namespacer_bind("Pavilion.Warcraft.Addons.Zen.Externals.MTALuaLinq.Enumerable", _mta_lualinq_enumerable)
+_namespacer_bind("Pavilion.Warcraft.Addons.Zen.Externals.ServiceLocators.LibStub", _libstub_service_locator)
