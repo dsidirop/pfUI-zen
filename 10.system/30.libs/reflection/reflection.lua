3a24543591046090107f4424592a37b74d0999b8
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-12-19 01:57:10 +0100

    fix (reflection.lua): add IsInteger*() methods

--- 10.system/30.libs/reflection/reflection.lua
+++ 10.system/30.libs/reflection/reflection.lua
@@ -1,4 +1,4 @@
-﻿local _type, _setfenv, _namespacer = (function()
+﻿local _type, _setfenv, _importer, _namespacer = (function()
     local _g = assert(_G or getfenv(0))
     local _assert = assert
     local _setfenv = _assert(_g.setfenv)
@@ -6,12 +6,15 @@
     _setfenv(1, {})
 
     local _type = _assert(_g.type)
+    local _importer = _assert(_g.pvl_namespacer_get)
     local _namespacer = _assert(_g.pvl_namespacer_add)
 
-    return _type, _setfenv, _namespacer
+    return _type, _setfenv, _importer, _namespacer
 end)()
 
 _setfenv(1, {})
+
+local Math = _import("System.Math")
 
 local Reflection = _namespacer("System.Reflection")
 
@@ -31,6 +34,14 @@ end
 
 function Reflection.IsNumberOrNil(value)
     return value == nil or _type(value) == "number"
+end
+
+function Reflection.IsInteger(value)
+    return Reflection.IsNumber(value) and Math.floor(value) == value
+end
+
+function Reflection.IsIntegerOrNil(value)
+    return value == nil or Reflection.IsInteger(value)
 end
 
 function Reflection.IsString(value)
