02f86b7209d6b060a746cfc033941e35ca7f1c38
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2023-12-29 16:05:54 +0100

    clean (scopify.lua)

--- 10.system/20.core/scopify/scopify.lua
+++ 10.system/20.core/scopify/scopify.lua
@@ -1,17 +1,10 @@
-﻿local _setfenv, _namespacer_bind = (function()
-    local _g = assert(_G or getfenv(0))
-    local _assert = assert
-    local _setfenv = _assert(_g.setfenv)
-    _setfenv(1, {})
+﻿local g = assert(_G or getfenv(0))
+local Bind = g.pvl_namespacer_bind
+local SetFunctionEnvironment = g.setfenv
 
-    local _namespacer_bind = _assert(_g.pvl_namespacer_bind)
-    
-    return _setfenv, _namespacer_bind
-end)()
-
-_namespacer_bind("System.EScopes", {
+Bind("System.EScopes", {
     EGlobal = 0,
     EFunction = 1,
 })
 
-_namespacer_bind("System.Scopify", _setfenv)
+Bind("System.Scopify", SetFunctionEnvironment)
