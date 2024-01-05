fad0f10241b7a44029351c58fa2d1342c8dfcf20
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2024-01-06 00:19:51 +0100

    clean (strings.isdudstring.lua)

--- 10.system/20.core/helpers/strings.isdudstring.lua
+++ 10.system/20.core/helpers/strings.isdudstring.lua
@@ -1,22 +1,10 @@
-local _setfenv, _importer, _namespacer = (function()
-    local _g = assert(_G or getfenv(0))
-    local _assert = assert
-    local _setfenv = _assert(_g.setfenv)
-    _setfenv(1, {})
+local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get)
 
-    local _importer = _assert(_g.pvl_namespacer_get)
-    local _namespacer = _assert(_g.pvl_namespacer_add)
+local Guard = using "System.Guard"
+local Scopify = using "System.Scopify"
+local EScopes = using "System.EScopes"
 
-    return _setfenv, _importer, _namespacer
-end)()
-
-_setfenv(1, {})
-
-local Guard = _importer("System.Guard")
-local Scopify = _importer("System.Scopify")
-local EScopes = _importer("System.EScopes")
-
-local StringsHelper = _namespacer("System.Helpers.Strings [Partial]")
+local StringsHelper = using "[declare]" "System.Helpers.Strings [Partial]"
 
 function StringsHelper.IsDudString(value)
     Scopify(EScopes.Function, StringsHelper)
