0d0ddb85be63d1958751b8919fff0fd7b708b59c
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2024-01-06 00:45:03 +0100

    clean (strings.stringify.lua)

--- 10.system/20.core/helpers/strings.stringify.lua
+++ 10.system/20.core/helpers/strings.stringify.lua
@@ -1,26 +1,7 @@
-local _setfenv, _tostring, _importer, _namespacer = (function()
-    local _g = assert(_G or getfenv(0))
-    local _assert = assert
-    local _setfenv = _assert(_g.setfenv)
-    _setfenv(1, {})
+local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get)
 
-    local _tostring = _assert(_g.tostring)
+local B = using "[built-ins]" [[   ToString = tostring   ]]
 
-    local _importer = _assert(_g.pvl_namespacer_get)
-    local _namespacer = _assert(_g.pvl_namespacer_add)
+local StringsHelper = using "[declare]" "System.Helpers.Strings [Partial]"
 
-    return _setfenv, _tostring, _importer, _namespacer
-end)()
-
-_setfenv(1, {})
-
-local Scopify = _importer("System.Scopify")
-local EScopes = _importer("System.EScopes")
-
-local StringsHelper = _namespacer("System.Helpers.Strings [Partial]")
-
-function StringsHelper.Stringify(value)
-    Scopify(EScopes.Function, StringsHelper)
-   
-    return _tostring(value)
-end
+StringsHelper.Stringify = B.ToString
