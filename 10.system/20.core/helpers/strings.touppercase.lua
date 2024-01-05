69511e348ea072a5c5055f99da0d53921e0fe2ee
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2024-01-06 00:47:18 +0100

    clean (strings.touppercase.lua)

--- 10.system/20.core/helpers/strings.touppercase.lua
+++ 10.system/20.core/helpers/strings.touppercase.lua
@@ -1,28 +1,7 @@
-local _setfenv, _strupper, _importer, _namespacer = (function()
-    local _g = assert(_G or getfenv(0))
-    local _assert = assert
-    local _setfenv = _assert(_g.setfenv)
-    _setfenv(1, {})
+local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get)
 
-    local _strupper = _assert(_g.string.upper)
-    local _importer = _assert(_g.pvl_namespacer_get)
-    local _namespacer = _assert(_g.pvl_namespacer_add)
+local B = using "[built-ins]" [[    StringToUppercase = string.upper    ]]
 
-    return _setfenv, _strupper, _importer, _namespacer
-end)()
+local StringsHelper = using "[declare]" "System.Helpers.Strings [Partial]"
 
-_setfenv(1, {})
-
-local Guard = _importer("System.Guard")
-local Scopify = _importer("System.Scopify")
-local EScopes = _importer("System.EScopes")
-
-local StringsHelper = _namespacer("System.Helpers.Strings [Partial]")
-
-function StringsHelper.ToUppercase(input)
-    Scopify(EScopes.Function, StringsHelper)
-
-    Guard.Assert.IsString(input, "input")
-    
-    return _strupper(input)
-end
+StringsHelper.ToUppercase = B.StringToUppercase
