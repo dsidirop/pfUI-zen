d9e35ed8d795f4f77ccab5b2b27b74681e25e285
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-05-13 19:03:16 +0200

    clean (gambled.item.info.tests.constructor.givenbasicinvalidparameters.shouldproperlyerrorout.lua): trivial neutral reformating

--- 20.externals/21.standard.tests/wow.testbed/group.looting.testbed/contracts.testbed/gambled.item.info.tests.constructor.givenbasicinvalidparameters.shouldproperlyerrorout.lua
+++ 20.externals/21.standard.tests/wow.testbed/group.looting.testbed/contracts.testbed/gambled.item.info.tests.constructor.givenbasicinvalidparameters.shouldproperlyerrorout.lua
@@ -5,7 +5,7 @@ local EWowItemQuality                        = using "Pavilion.Warcraft.Enums.EW
 local GambledItemInfoDto                     = using "Pavilion.Warcraft.GroupLooting.Contracts.GambledItemInfoDto"
 
 local ValueIsOutOfRangeException             = using "System.Exceptions.ValueIsOutOfRangeException"
-local ArgumentHasInappropriateTypeException  = using "System.Exceptions.ValueIsOfInappropriateTypeException"
+local ValueHasInappropriateTypeException     = using "System.Exceptions.ValueIsOfInappropriateTypeException"
 
 local TG, U = using "[testgroup.tagged]" "Pavilion.Warcraft.GroupLooting.Contracts.GambledItemInfoDto.Tests" { "pavilion", "grouplooting" } -- @formatter:on
 
@@ -13,74 +13,74 @@ TG:AddDynamicTheory("GambledItemInfoDto.Constructor.GivenBasicInvalidParameters.
         function()
             return {
                 ["GII.CTOR.GBIP.SPEO.010"] = {
-                    Name = nil,
-                    GamblingId = 123,
-                    ItemQuality = EWowItemQuality.Green,
+                    Name           = nil,
+                    GamblingId     = 123,
+                    ItemQuality    = EWowItemQuality.Green,
                     IsBindOnPickUp = false,
                 },
                 ["GII.CTOR.GBIP.SPEO.020"] = {
-                    Name = "",
-                    GamblingId = 123,
-                    ItemQuality = EWowItemQuality.Green,
+                    Name           = "",
+                    GamblingId     = 123,
+                    ItemQuality    = EWowItemQuality.Green,
                     IsBindOnPickUp = false,
                 },
                 ["GII.CTOR.GBIP.SPEO.030"] = {
-                    Name = "   ",
-                    GamblingId = 123,
-                    ItemQuality = EWowItemQuality.Green,
+                    Name           = "   ",
+                    GamblingId     = 123,
+                    ItemQuality    = EWowItemQuality.Green,
                     IsBindOnPickUp = false,
                 },
                 ["GII.CTOR.GBIP.SPEO.040"] = {
-                    Name = "Foobar",
-                    GamblingId = -1,
-                    ItemQuality = EWowItemQuality.Green,
+                    Name           = "Foobar",
+                    GamblingId     = -1,
+                    ItemQuality    = EWowItemQuality.Green,
                     IsBindOnPickUp = false,
                 },
                 ["GII.CTOR.GBIP.SPEO.050"] = {
-                    Name = "Foobar",
-                    GamblingId = 1,
-                    ItemQuality = nil,
+                    Name           = "Foobar",
+                    GamblingId     = 1,
+                    ItemQuality    = nil,
                     IsBindOnPickUp = false,
                 },
                 ["GII.CTOR.GBIP.SPEO.060"] = {
-                    Name = "Foobar",
-                    GamblingId = 1,
-                    ItemQuality = -1,
+                    Name           = "Foobar",
+                    GamblingId     = 1,
+                    ItemQuality    = -1,
                     IsBindOnPickUp = false,
                 },
                 ["GII.CTOR.GBIP.SPEO.070"] = {
-                    Name = "Foobar",
-                    GamblingId = 1,
-                    ItemQuality = 99, -- <-- this should trigger an error because the value is too high and its way too suspicious for us to allow it 
+                    Name           = "Foobar",
+                    GamblingId     = 1,
+                    ItemQuality    = 99, -- <-- this should trigger an error because the value is too high and its way too suspicious for us to allow it 
                     IsBindOnPickUp = false,
                 },
                 ["GII.CTOR.GBIP.SPEO.080"] = {
-                    Name = "Foobar",
-                    GamblingId = 1,
-                    ItemQuality = EWowItemQuality.Green,
+                    Name           = "Foobar",
+                    GamblingId     = 1,
+                    ItemQuality    = EWowItemQuality.Green,
                     IsBindOnPickUp = { },
                 },
                 ["GII.CTOR.GBIP.SPEO.090"] = {
-                    Name = "Foobar",
-                    GamblingId = 123,
-                    ItemQuality = EWowItemQuality.Green,
+                    Name           = "Foobar",
+                    GamblingId     = 123,
+                    ItemQuality    = EWowItemQuality.Green,
                     IsBindOnPickUp = false,
 
-                    Count = 99999, -- <-- way too high so it should trigger an error
+                    Count          = 99999, -- <-- way too high so it should trigger an error
                 },
                 ["GII.CTOR.GBIP.SPEO.100"] = {
-                    Name = "Foobar",
-                    GamblingId = 123,
-                    ItemQuality = EWowItemQuality.Green,
-                    IsBindOnPickUp = false,
+                    Name            = "Foobar",
+                    GamblingId      = 123,
+                    ItemQuality     = EWowItemQuality.Green,
+                    IsBindOnPickUp  = false,
 
                     TextureFilepath = "", -- <-- this should trigger an error because its not a valid texture filepath
                 },
                 ["GII.CTOR.GBIP.SPEO.110"] = {
-                    Name = "Foobar",
-                    GamblingId = 123,
-                    ItemQuality = EWowItemQuality.Green,
-                    IsBindOnPickUp = false,
+                    Name            = "Foobar",
+                    GamblingId      = 123,
+                    ItemQuality     = EWowItemQuality.Green,
+                    IsBindOnPickUp  = false,
 
                     TextureFilepath = "  ", -- <-- this should also trigger an error because its not a valid texture filepath
                 },
@@ -90,24 +90,24 @@ TG:AddDynamicTheory("GambledItemInfoDto.Constructor.GivenBasicInvalidParameters.
             -- ARRANGE
             local properExceptionThrown = false
 
-            -- ACT   todo  introduce fluent assertions
-            local gambledItemInfo = Try(function() --@formatter:off
+            -- ACT
+            local gambledItemInfo = Try:New(function() --@formatter:off
                 return GambledItemInfoDto:New(options)
             end)
             :Catch(ValueIsOutOfRangeException, function()
-                -- using "System.Console").Out:WriteFormatted("%s", ex)
+                -- (using "System.Console").Out:WriteFormatted("%s", ex)
                 
                 properExceptionThrown = true
             end)
-            :Catch(ArgumentHasInappropriateTypeException, function()
-                -- using "System.Console").Out:WriteFormatted("%s", ex)
+            :Catch(ValueHasInappropriateTypeException, function()
+                -- (using "System.Console").Out:WriteFormatted("%s", ex)
                 
                 properExceptionThrown = true
             end)
             :Run() --@formatter:on
 
             -- ASSERT
-            U.Should.Be.Truthy(gambledItemInfo == nil)
+            U.Should.Be.Nil(gambledItemInfo)
             U.Should.Be.Truthy(properExceptionThrown)
         end
 )
