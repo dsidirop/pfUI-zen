2094c054bde530f1882c512a4b25820e1ac96889
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-05-14 02:05:14 +0200

    feat (enums.tests.lua): scaffold enums-tests

---
+++ 10.system/21.core.tests/enums.testbed/enums.tests.lua
@@ -0,0 +1,29 @@
+﻿local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get)
+
+local Scopify = using "System.Scopify"
+local EScopes = using "System.EScopes"
+
+local U = using "[built-in]" [[ VWoWUnit ]]
+
+local TestsGroup = U.TestsEngine:CreateOrUpdateGroup {
+    Name = "System.Enums",
+    Tags = { "system", "enums" },
+}
+
+Scopify(EScopes.Function, {})
+
+TestsGroup:AddFact("T000.Enum.GivenAttemptToAccessNonExistingMember.ShouldThrow",
+        function()
+            -- ARRANGE
+
+            -- ACT
+            function action()
+                local EFoo = using "[declare] [enum]" "T000.Enum.GivenAttemptToAccessNonExistingMember.ShouldThrow.EFoo"
+
+                local _ = EFoo.Foo -- doesnt exist obviously so this should throw
+            end
+
+            -- ASSERT
+            U.Should.Throw(action)
+        end
+)
