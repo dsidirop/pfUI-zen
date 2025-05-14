0a9085e8c81e63c5e3bb2ecd36a3034a4f7209cb
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-05-14 02:50:13 +0200

    feat (enums.tests.lua): add more tests

--- 10.system/21.core.tests/enums.testbed/enums.tests.lua
+++ 10.system/21.core.tests/enums.testbed/enums.tests.lua
@@ -12,7 +12,7 @@ local TestsGroup = U.TestsEngine:CreateOrUpdateGroup {
 
 Scopify(EScopes.Function, {})
 
-TestsGroup:AddFact("T000.Enum.GivenAttemptToAccessNonExistingMember.ShouldThrow",
+TestsGroup:AddFact("T000.EnumLookup.GivenAttemptToAccessExistingMember.ShouldNotThrow",
         function()
             -- ARRANGE
 
@@ -20,10 +20,87 @@ TestsGroup:AddFact("T000.Enum.GivenAttemptToAccessNonExistingMember.ShouldThrow"
             function action()
                 local EFoo = using "[declare] [enum]" "T000.Enum.GivenAttemptToAccessNonExistingMember.ShouldThrow.EFoo"
 
-                local _ = EFoo.Foo -- doesnt exist obviously so this should throw
+                EFoo.Bar = 1
+                
+                local _ = EFoo.Bar
+            end
+
+            -- ASSERT
+            U.Should.Not.Throw(action)
+        end
+)
+
+TestsGroup:AddFact("T050.EnumLookup.GivenAttemptToAccessNonExistingMember.ShouldThrow",
+        function()
+            -- ARRANGE
+
+            -- ACT
+            function action()
+                local EFoo = using "[declare] [enum]" "T000.Enum.GivenAttemptToAccessNonExistingMember.ShouldThrow.EFoo"
+                
+                -- EFoo.Bar = 1
+
+                local _ = EFoo.Bar -- doesnt exist obviously so this should throw
             end
 
             -- ASSERT
             U.Should.Throw(action)
+        end
+)
+
+TestsGroup:AddTheory("T010.EnumBuiltInIsValidMethod.GivenInvalidValueToCheck.ShouldReturnExpectedVerdict",
+        {
+            ["EBIIVM.GIVTC.SRER.0000"] = { Value = -1, ExpectedVerdict = false },
+            ["EBIIVM.GIVTC.SRER.0010"] = { Value = 0, ExpectedVerdict = false },
+            ["EBIIVM.GIVTC.SRER.0020"] = { Value = 1, ExpectedVerdict = true },
+            ["EBIIVM.GIVTC.SRER.0030"] = { Value = 2, ExpectedVerdict = true },
+            ["EBIIVM.GIVTC.SRER.0040"] = { Value = 3, ExpectedVerdict = false },
+            ["EBIIVM.GIVTC.SRER.0050"] = { Value = 4, ExpectedVerdict = false },
+            ["EBIIVM.GIVTC.SRER.0060"] = { Value = 5, ExpectedVerdict = false },
+        },
+        function(options)
+            -- ARRANGE
+
+            -- ACT
+            function action()
+                local EFoo = using "[declare] [enum]" "T010.EnumBuiltInIsValidMethod.GivenInvalidValueToCheck.ShouldReturnExpectedVerdict.EFoo"
+
+                EFoo.A = 1
+                EFoo.B = 2
+                
+                return EFoo:IsValid(options.Value)
+            end
+
+            -- ASSERT
+            local result = U.Should.Not.Throw(action)
+            
+            U.Should.Be.PlainlyEqual(result, options.ExpectedVerdict)
+        end
+)
+
+TestsGroup:AddTheory("T020.StringEnumBuiltInIsValidMethod.GivenInvalidValueToCheck.ShouldReturnExpectedVerdict",
+        {
+            ["SEBIIVM.GIVTC.SRER.0000"] = { Value = "_", ExpectedVerdict = false },
+            ["SEBIIVM.GIVTC.SRER.0020"] = { Value = "a", ExpectedVerdict = true },
+            ["SEBIIVM.GIVTC.SRER.0030"] = { Value = "b", ExpectedVerdict = true },
+            ["SEBIIVM.GIVTC.SRER.0040"] = { Value = ".", ExpectedVerdict = false },
+        },
+        function(options)
+            -- ARRANGE
+
+            -- ACT
+            function action()
+                local EFoo = using "[declare] [enum]" "T020.StringEnumBuiltInIsValidMethod.GivenInvalidValueToCheck.ShouldReturnExpectedVerdict.EFoo"
+
+                EFoo.A = "a"
+                EFoo.B = "b"
+
+                return EFoo:IsValid(options.Value)
+            end
+
+            -- ASSERT
+            local result = U.Should.Not.Throw(action)
+
+            U.Should.Be.PlainlyEqual(result, options.ExpectedVerdict)
         end
 )
