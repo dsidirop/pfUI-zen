dd5ccc9ad0438a860e8593abaf9d2024abcaf6c9
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-05-18 03:11:30 +0200

    feat (strings.starts.with.lua): new string-utility

---
+++ 10.system/20.core/helpers.strings/strings.starts.with.lua
@@ -0,0 +1,48 @@
+local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get) -- @formatter:off
+
+local B = using "[built-ins]"    [[
+    StringSub    = string.sub,
+    StringFind   = string.find,
+    StringLength = string.len,
+]]
+
+local StringSub    = B.StringSub
+local StringFind   = B.StringFind
+local StringLength = B.StringLength
+
+local Guard   = using "System.Guard"
+local Scopify = using "System.Scopify"
+local EScopes = using "System.EScopes" -- @formatter:on
+
+local StringsHelper = using "[declare] [static]" "System.Helpers.Strings [Partial]"
+
+function StringsHelper.StartsWith(input, desiredPrefix)
+    Scopify(EScopes.Function, StringsHelper)
+
+    Guard.Assert.IsString(input, "input")
+    Guard.Assert.IsString(desiredPrefix, "desiredPrefix")
+
+    if desiredPrefix == "" then
+        return true
+    end
+
+    if input == "" then
+        return false
+    end
+
+    local desiredPrefixLength = StringLength(desiredPrefix)
+    if StringLength(input) < desiredPrefixLength then
+        return false
+    end
+
+    if input == desiredPrefix then
+        return true
+    end
+
+    if desiredPrefixLength == 1 then
+        -- optimization for 1-char-prefixes because for the runtime those 1-char-prefixes are very efficiently cached via string-interning
+        return StringSub(input, 1, 1) == desiredPrefix
+    end
+
+    return StringFind(input, desiredPrefix, --[[startindex:]] 1, --[[plainsearch:]] true) == 1
+end
---
+++ 10.system/21.core.tests/strings.helper.testbed/strings.helper.starts.with.tests.lua
@@ -0,0 +1,71 @@
+﻿local U, _setfenv, _importer = (function()
+    local _g = assert(_G or getfenv(0))
+    local _assert = assert
+    local _setfenv = _assert(_g.setfenv)
+    _setfenv(1, {})
+
+    local U = _assert(_g.VWoWUnit)
+    local _importer = _assert(_g.pvl_namespacer_get)
+
+    return U, _setfenv, _importer
+end)()
+
+_setfenv(1, {})
+
+local StringsHelper = _importer("System.Helpers.Strings")
+
+local TestsGroup = U.TestsEngine:CreateOrUpdateGroup { Name = "System.Helpers.Strings" }
+
+TestsGroup:AddTheory("StringsHelper.StartsWith.GivenVariousInputs.ShouldMatchExpectedResults",
+        {
+            ["SH.SW.GVI.SMER.0000"] = {
+                Input = "abc",
+                Prefix = "abc",
+                ExpectedResult = true,
+            },
+            ["SH.SW.GVI.SMER.0010"] = {
+                Input = "abc",
+                Prefix = "ab",
+                ExpectedResult = true,
+            },
+            ["SH.SW.GVI.SMER.0020"] = {
+                Input = "abc",
+                Prefix = "a",
+                ExpectedResult = true,
+            },
+            ["SH.SW.GVI.SMER.0030"] = {
+                Input = "abc",
+                Prefix = "abcd",
+                ExpectedResult = false,
+            },
+            ["SH.SW.GVI.SMER.0040"] = {
+                Input = "abc",
+                Prefix = "",
+                ExpectedResult = true,
+            },
+            ["SH.SW.GVI.SMER.0050"] = {
+                Input = "",
+                Prefix = "",
+                ExpectedResult = true,
+            },
+            ["SH.SW.GVI.SMER.0060"] = {
+                Input = "",
+                Prefix = "a",
+                ExpectedResult = false,
+            },
+            ["SH.SW.GVI.SMER.0070"] = {
+                Input = "ab",
+                Prefix = "b",
+                ExpectedResult = false,
+            },
+        },
+        function(options)
+            -- ARRANGE
+
+            -- ACT
+            local result = StringsHelper.StartsWith(options.Input, options.Prefix)
+
+            -- ASSERT
+            U.Should.Be.PlainlyEqual(result, options.ExpectedResult)
+        end
+)
