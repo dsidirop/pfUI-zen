38c776370f2dc900db1f33f613ff2aab43d3f5b6
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-05-21 22:53:44 +0200

    clean (repository.writable.lua): migrate over to the _.EnrichInstanceWithFields() style

--- 30.zen.module/zen.persistence/units.of.work/settings/user.preferences/repository.writable.lua
+++ 30.zen.module/zen.persistence/units.of.work/settings/user.preferences/repository.writable.lua
@@ -1,32 +1,15 @@
-﻿local _assert, _setfenv, _type, _getn, _error, _print, _unpack, _pairs, _importer, _namespacer, _setmetatable = (function()
-    local _g = assert(_G or getfenv(0))
-    local _assert = assert
-    local _setfenv = _assert(_g.setfenv)
+﻿local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get) -- @formatter:off
 
-    _setfenv(1, {})
+local Guard = using "System.Guard"
+local Scopify = using "System.Scopify"
+local EScopes = using "System.EScopes"
 
-    local _type = _assert(_g.type)
-    local _getn = _assert(_g.table.getn)
-    local _error = _assert(_g.error)
-    local _print = _assert(_g.print)
-    local _pairs = _assert(_g.pairs)
-    local _unpack = _assert(_g.unpack)
-    local _importer = _assert(_g.pvl_namespacer_get)
-    local _namespacer = _assert(_g.pvl_namespacer_add)
-    local _setmetatable = _assert(_g.setmetatable)
+local SGreeniesGrouplootingAutomationMode = using "Pavilion.Warcraft.Addons.Zen.Foundation.Contracts.Strenums.SGreeniesGrouplootingAutomationMode"
+local SGreeniesGrouplootingAutomationActOnKeybind = using "Pavilion.Warcraft.Addons.Zen.Foundation.Contracts.Strenums.SGreeniesGrouplootingAutomationActOnKeybind"
 
-    return _assert, _setfenv, _type, _getn, _error, _print, _unpack, _pairs, _importer, _namespacer, _setmetatable
-end)()
+local Class = using "[declare]" "Pavilion.Warcraft.Addons.Zen.Persistence.Settings.UserPreferences.RepositoryWriteable" -- @formatter:on
 
-_setfenv(1, {})
-
-local Scopify = _importer("System.Scopify")
-local EScopes = _importer("System.EScopes")
-
-local SGreeniesGrouplootingAutomationMode = _importer("Pavilion.Warcraft.Addons.Zen.Foundation.Contracts.Strenums.SGreeniesGrouplootingAutomationMode")
-local SGreeniesGrouplootingAutomationActOnKeybind = _importer("Pavilion.Warcraft.Addons.Zen.Foundation.Contracts.Strenums.SGreeniesGrouplootingAutomationActOnKeybind")
-
-local Class = _namespacer("Pavilion.Warcraft.Addons.Zen.Persistence.Settings.UserPreferences.RepositoryWriteable")
+Scopify(EScopes.Function, {})
 
 function Class._.EnrichInstanceWithFields(upcomingInstance)
     upcomingInstance._hasChanges = false
@@ -38,7 +21,7 @@ end
 function Class:New(dbcontext)
     Scopify(EScopes.Function, self)
 
-    _assert(_type(dbcontext) == "table")
+    Guard.Assert.IsTable(dbcontext, "dbcontext")
 
     local instance = self:Instantiate()
     
@@ -54,11 +37,11 @@ function Class:HasChanges()
     return _hasChanges
 end
 
--- @return self
+--- @return self
 function Class:GreeniesGrouplootingAutomation_ChainUpdateMode(value)
     Scopify(EScopes.Function, self)
-    
-    _assert(SGreeniesGrouplootingAutomationMode:IsValid(value))
+
+    Guard.Assert.IsEnumValue(SGreeniesGrouplootingAutomationMode, value, "value")
     
     if _userPreferencesEntity.GreeniesGrouplootingAutomation.Mode == value then
         return self
@@ -70,11 +53,11 @@ function Class:GreeniesGrouplootingAutomation_ChainUpdateMode(value)
     return self
 end
 
--- @return self
+--- @return self
 function Class:GreeniesGrouplootingAutomation_ChainUpdateActOnKeybind(value)
     Scopify(EScopes.Function, self)
 
-    _assert(SGreeniesGrouplootingAutomationActOnKeybind:IsValid(value))
+    Guard.Assert.IsEnumValue(SGreeniesGrouplootingAutomationActOnKeybind, value, "value")
 
     if _userPreferencesEntity.GreeniesGrouplootingAutomation.ActOnKeybind == value then
         return self
