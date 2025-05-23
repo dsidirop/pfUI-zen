75876e4e77fe6de94183f778acc9429b7565486a
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-05-24 01:18:10 +0200

    feat (service.queryable.lua): add EnrichInstanceWithFields function for instance initialization

--- 30.zen.module/zen.persistence/services/addon.settings/user.preferences/service.queryable.lua
+++ 30.zen.module/zen.persistence/services/addon.settings/user.preferences/service.queryable.lua
@@ -1,37 +1,31 @@
-﻿local _assert, _setfenv, _type, _getn, _error, _print, _unpack, _pairs, _importer, _namespacer, _setmetatable = (function()
-    local _g = assert(_G or getfenv(0))
-    local _assert = assert
-    local _setfenv = _assert(_g.setfenv)
+﻿local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get)
 
-    _setfenv(1, {})
+local Nils = using "System.Nils"
+local Guard = using "System.Guard"
+local Scopify = using "System.Scopify"
+local EScopes = using "System.EScopes"
+local UserPreferencesRepositoryQueryable = using "Pavilion.Warcraft.Addons.Zen.Persistence.Settings.UserPreferences.RepositoryQueryable"
 
-    local _type = _assert(_g.type)
-    local _getn = _assert(_g.table.getn)
-    local _error = _assert(_g.error)
-    local _print = _assert(_g.print)
-    local _pairs = _assert(_g.pairs)
-    local _unpack = _assert(_g.unpack)
-    local _importer = _assert(_g.pvl_namespacer_get)
-    local _namespacer = _assert(_g.pvl_namespacer_add)
-    local _setmetatable = _assert(_g.setmetatable)
+local Class = using "[declare]" "Pavilion.Warcraft.Addons.Zen.Persistence.Services.AddonSettings.UserPreferences.QueryableService"
 
-    return _assert, _setfenv, _type, _getn, _error, _print, _unpack, _pairs, _importer, _namespacer, _setmetatable
-end)()
+Scopify(EScopes.Function, {})
 
-_setfenv(1, {})
+function Class._.EnrichInstanceWithFields(upcomingInstance)
+    upcomingInstance._userPreferencesRepositoryQueryable = nil
 
-local Scopify = _importer("System.Scopify")
-local EScopes = _importer("System.EScopes")
-local UserPreferencesRepositoryQueryable = _importer("Pavilion.Warcraft.Addons.Zen.Persistence.Settings.UserPreferences.RepositoryQueryable")
-
-local Class = _namespacer("Pavilion.Warcraft.Addons.Zen.Persistence.Services.AddonSettings.UserPreferences.QueryableService")
+    return upcomingInstance
+end
 
 function Class:New(userPreferencesRepositoryQueryable)
     Scopify(EScopes.Function, self)
 
-    return self:Instantiate({
-        _userPreferencesRepositoryQueryable = userPreferencesRepositoryQueryable or UserPreferencesRepositoryQueryable:New(), --todo   refactor this later on so that this gets injected through DI
-    })
+    Guard.Assert.IsNilOrTable(userPreferencesRepositoryQueryable, "userPreferencesRepositoryQueryable")
+
+    local instance = self:Instantiate()
+
+    instance._userPreferencesRepositoryQueryable = Nils.Coalesce(userPreferencesRepositoryQueryable, UserPreferencesRepositoryQueryable:New()) --todo   refactor this later on so that this gets injected through DI
+
+    return instance
 end
 
 function Class:GetAllUserPreferences()
