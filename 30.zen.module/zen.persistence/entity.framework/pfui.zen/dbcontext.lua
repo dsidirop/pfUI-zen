42174ac48b147f86ce32fbf07411c75a7a0a0807
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-05-24 01:33:10 +0200

    feat(dbcontext.lua): implement EnrichInstanceWithFields for instance initialization

--- 30.zen.module/zen.persistence/entity.framework/pfui.zen/dbcontext.lua
+++ 30.zen.module/zen.persistence/entity.framework/pfui.zen/dbcontext.lua
@@ -1,57 +1,49 @@
-﻿local _assert, _setfenv, _type, _getn, _error, _print, _unpack, _pairs, _importer, _namespacer, _setmetatable = (function()
-    local _g = assert(_G or getfenv(0))
-    local _assert = assert
-    local _setfenv = _assert(_g.setfenv)
+﻿local using = assert((_G or getfenv(0) or {}).pvl_namespacer_get)
 
-    _setfenv(1, {})
+local Nils = using "System.Nils"
+local Scopify = using "System.Scopify"
+local EScopes = using "System.EScopes"
 
-    local _type = _assert(_g.type)
-    local _getn = _assert(_g.table.getn)
-    local _error = _assert(_g.error)
-    local _print = _assert(_g.print)
-    local _pairs = _assert(_g.pairs)
-    local _unpack = _assert(_g.unpack)
-    local _importer = _assert(_g.pvl_namespacer_get)
-    local _namespacer = _assert(_g.pvl_namespacer_add)
-    local _setmetatable = _assert(_g.setmetatable)
+local Schema = using "Pavilion.Warcraft.Addons.Zen.Persistence.EntityFramework.Pfui.Zen.Schemas.SchemaV1"
+local PfuiConfiguration = using "Pavilion.Warcraft.Addons.Zen.Externals.Pfui.Configuration"
 
-    return _assert, _setfenv, _type, _getn, _error, _print, _unpack, _pairs, _importer, _namespacer, _setmetatable
-end)()
+local Class = using "[declare]" "Pavilion.Warcraft.Addons.Zen.Persistence.EntityFramework.PfuiZen.DBContext"
 
-_setfenv(1, {})
+Scopify(EScopes.Function, {})
 
-local Scopify = _importer("System.Scopify")
-local EScopes = _importer("System.EScopes")
+function Class._.EnrichInstanceWithFields(upcomingInstance)
+    upcomingInstance.Settings = { -- these are all public properties
+        Logging         = { --[[placeholder]] },
+        EngineSettings  = { --[[placeholder]] },
+        UserPreferences = {
+            GreeniesGrouplootingAutomation = {
+                Mode         = nil,
+                ActOnKeybind = nil,
+            },
+        },
+    }
 
-local Schema = _importer("Pavilion.Warcraft.Addons.Zen.Persistence.EntityFramework.Pfui.Zen.Schemas.SchemaV1")
-local PfuiConfiguration = _importer("Pavilion.Warcraft.Addons.Zen.Externals.Pfui.Configuration")
-
-local Class = _namespacer("Pavilion.Warcraft.Addons.Zen.Persistence.EntityFramework.PfuiZen.DBContext")
+    return upcomingInstance
+end
 
 function Class:New()
     Scopify(EScopes.Function, self)
-    
+
     local rawAddonSettings = PfuiConfiguration[Schema.RootKeyname] or {} -- pfUI.env.C["zen.v1"]
 
-    return self:Instantiate({ -- @formatter:off
+    local instance = self:Instantiate()
 
-        Settings = { --        these are all public properties
-            Logging = {
-                -- placeholder
-            },
+    instance
+            .Settings
+            .UserPreferences
+            .GreeniesGrouplootingAutomation.Mode = Nils.Coalesce(rawAddonSettings[Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.Mode.Keyname], Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.Mode.Default)
 
-            EngineSettings = {
-                -- placeholder
-            },
+    instance
+            .Settings
+            .UserPreferences
+            .GreeniesGrouplootingAutomation.ActOnKeybind = Nils.Coalesce(rawAddonSettings[Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.ActOnKeybind.Keyname], Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.ActOnKeybind.Default)
 
-            UserPreferences = {
-                GreeniesGrouplootingAutomation = {
-                    Mode         = (rawAddonSettings[Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.Mode.Keyname]         or Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.Mode.Default),
-                    ActOnKeybind = (rawAddonSettings[Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.ActOnKeybind.Keyname] or Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.ActOnKeybind.Default),
-                },
-            },
-        },
-    }) -- @formatter:on
+    return instance
 end
 
 function Class:SaveChanges()
@@ -63,6 +55,6 @@ function Class:SaveChanges()
     rawAddonSettings[Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.Mode.Keyname]         = Settings.UserPreferences.GreeniesGrouplootingAutomation.Mode
     rawAddonSettings[Schema.Settings.UserPreferences.GreeniesGrouplootingAutomation.ActOnKeybind.Keyname] = Settings.UserPreferences.GreeniesGrouplootingAutomation.ActOnKeybind
     -- @formatter:on
-    
+
     PfuiConfiguration[Schema.RootKeyname] = rawAddonSettings
 end
