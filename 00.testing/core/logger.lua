2b422fe6a2e7b00d2b6a5b0538e2a9b9fab8ea1e
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-06-01 19:02:57 +0200

    refa (logger.lua): optimize printing so that it will print using wow-default-chat-frame if available, falling back to 'print' if it is not available

--- 00.testing/core/logger.lua
+++ 00.testing/core/logger.lua
@@ -1,4 +1,4 @@
-local VWoWUnit, ELogLevels, _assert, _type, _print, _tostring, _setfenv, _setmetatable = (function()
+local VWoWUnit, _wowDefaultChatFrame, ELogLevels, _assert, _type, _print, _tostring, _setfenv, _setmetatable = (function()
 	local _g = assert(_G or getfenv(0))
 	local _assert = assert
 	local _setfenv = _assert(_g.setfenv)
@@ -11,8 +11,9 @@ local VWoWUnit, ELogLevels, _assert, _type, _print, _tostring, _setfenv, _setmet
 	local _print = _assert(_g.print)
     local _tostring = _assert(_g.tostring)
 	local _setmetatable = _assert(_g.setmetatable)
+    local _wowDefaultChatFrame = _g.DEFAULT_CHAT_FRAME -- dont assert this   it can be null
     
-	return _g.VWoWUnit, _ELogLevels, _assert, _type, _print, _tostring, _setfenv, _setmetatable
+	return _g.VWoWUnit, _wowDefaultChatFrame, _ELogLevels, _assert, _type, _print, _tostring, _setfenv, _setmetatable
 end)()
 
 _setfenv(1, {})
@@ -22,11 +23,15 @@ Logger.__index = Logger -- standard class-proto scaffolding
 
 function Logger:New(optionalLogLevel)
 	_setfenv(1, self)
-	
-	local instance = {
+
+    local instance = {
 		_options = {
 			minLogLevel = optionalLogLevel or ELogLevels.Info, -- by default will print success and failure messages of tests
-		}
+		},
+
+        _properPrintFunc = _type(_wowDefaultChatFrame) == "table" and _type(_wowDefaultChatFrame.AddMessage) == "function" -- @formatter:off
+                and (function(message) return _wowDefaultChatFrame:AddMessage(message) end)  --                                    if we are in wow client use the chat frame to print messages
+                or _print, -- failsafe                                                                                        @formatter:on
 	}
 
 	_setmetatable(instance, self)
@@ -54,58 +59,77 @@ function Logger:Log(message, atLevel)
 	
 	_assert(_type(atLevel) == "number", "level must be a number")
 	
-	if atLevel >= _options.minLogLevel then
-		_print(_tostring(message))
+	if atLevel < _options.minLogLevel then
+		return
 	end
+
+    self:Print_(_tostring(message))
 end
 
 function Logger:LogTrace(message)
 	_setfenv(1, self)
 
-	if ELogLevels.Trace >= _options.minLogLevel then
-		_print(_tostring(message))
+	if ELogLevels.Trace < _options.minLogLevel then
+        return
 	end
+
+    self:Print_(_tostring(message))
 end
 
 function Logger:LogDebug(message)
 	_setfenv(1, self)
 
-	if ELogLevels.Debug >= _options.minLogLevel then
-		_print(_tostring(message))
+	if ELogLevels.Debug < _options.minLogLevel then
+		return
 	end
+
+    self:Print_(_tostring(message))
 end
 
 function Logger:LogInfo(message)
 	_setfenv(1, self)
 	
-	if ELogLevels.Info >= _options.minLogLevel then
-		_print(_tostring(message))
+	if ELogLevels.Info < _options.minLogLevel then
+        return
 	end
+
+    self:Print_("|cff00ccff" .. _tostring(message))
 end
 
 function Logger:LogWarn(message)
 	_setfenv(1, self)
 
-	if ELogLevels.Warn >= _options.minLogLevel then
-		_print(_tostring(message))
+	if ELogLevels.Warn < _options.minLogLevel then
+        return
 	end
+
+    self:Print_("|cffFFC125" .. _tostring(message))
 end
 
 function Logger:LogError(message) -- when tests fail
 	_setfenv(1, self)
 
-	if ELogLevels.Error >= _options.minLogLevel then
-		_print(_tostring(message))
+	if ELogLevels.Error < _options.minLogLevel then
+        return
 	end
-end
 
+    self:Print_("|cffff0000" .. _tostring(message))
+end
 
 function Logger:LogFatal(message)
 	_setfenv(1, self)
 
-	if ELogLevels.Fatal >= _options.minLogLevel then
-		_print(_tostring(message))
+	if ELogLevels.Fatal < _options.minLogLevel then
+		return
 	end
+
+    self:Print_("|cffff0000" .. _tostring(message))
+end
+
+function Logger:Print_(message)
+    _setfenv(1, self)
+
+    _properPrintFunc(message)
 end
 
 VWoWUnit.DefaultLogger = Logger:New()
