2ea65fda16c2ee192712a9cd9942e39ecee8af06
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-06-14 13:39:24 +0200

    refa (pfui.configuration.lua): refactor binding to use "[declare] [bind]" syntax for PfuiConfiguration

--- 20.externals/10.standard/pfui/pfui.configuration/pfui.configuration.lua
+++ 20.externals/10.standard/pfui/pfui.configuration/pfui.configuration.lua
@@ -1,7 +1,5 @@
-local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"])
+--[[@formatter:off]] local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"]); local Scopify = using "System.Scopify"; local EScopes = using "System.EScopes"; Scopify(EScopes.Function, {}) --[[@formatter:on]]
 
-local B = using "[built-ins]" [[  PfuiConfiguration = pfUI.env.C  ]]
+local PfuiConfiguration = using "[built-in]" [[  pfUI.env.C  ]]
 
-local Namespacer = using "System.Namespacer"
-
-Namespacer:BindRawSymbol("Pavilion.Warcraft.Addons.Zen.Externals.Pfui.Configuration", B.PfuiConfiguration)
+using "[declare] [bind]" "Pavilion.Warcraft.Addons.Zen.Externals.Pfui.Configuration" (PfuiConfiguration)
