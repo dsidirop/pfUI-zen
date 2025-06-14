2aad87968e91991d2daf4643b5adab30d976617b
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-06-14 13:28:30 +0200

    refa (pfui.gui.lua): update binding for PfuiGui using "[declare] [bind]" syntax

--- 20.externals/10.standard/pfui/pfui.gui/namespace.lua
+++
@@ -1,7 +0,0 @@
-local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"])
-
-local B = using "[built-ins]" [[  PfuiGui = pfUI.gui  ]]
-
-local Namespacer = using "System.Namespacer"
-
-Namespacer:BindRawSymbol("Pavilion.Warcraft.Addons.Zen.Externals.Pfui.Gui", B.PfuiGui)
