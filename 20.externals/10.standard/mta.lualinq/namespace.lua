850744036d4726ab1271b6592222e5f439bfe501
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-06-15 15:37:27 +0200

    clean (enumerable.lua): simplify the impl of this binding using "[declare] [bind]"

--- 20.externals/10.standard/mta.lualinq/namespace.lua
+++
@@ -1,9 +0,0 @@
-local _g = assert(_G or getfenv(0))
-local _setfenv = assert(_g.setfenv)
-local _namespacer_binder = assert(_g["ZENSHARP:BIND_RAW_SYMBOL"])
-local _mta_lualinq_enumerable = assert(_g.Enumerable)
-
-_g = nil
-_setfenv(1, {})
-
-_namespacer_binder("Pavilion.Warcraft.Addons.Zen.Externals.MTALuaLinq.Enumerable", _mta_lualinq_enumerable)
