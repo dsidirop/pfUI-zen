811234ab37e2e51ee0e03f8a4152fbce4cf2fc4d
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-07-12 02:23:56 +0200

    feat (30.zen.module): refine the folder structure; namespaces will be refined a bit further down the road

---
+++ 30.zen.module/20.mediators/zen.mediators.contracts/commands/zen.engine/restart.engine.command.lua
@@ -0,0 +1,3 @@
+﻿--[[@formatter:off]] local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"]); local Scopify = using "System.Scopify"; local EScopes = using "System.EScopes"; Scopify(EScopes.Function, {}) --[[@formatter:on]]
+
+local Class = using "[declare]" "Pavilion.Warcraft.Addons.Zen.Controllers.Contracts.Commands.ZenEngine.RestartEngineCommand"
