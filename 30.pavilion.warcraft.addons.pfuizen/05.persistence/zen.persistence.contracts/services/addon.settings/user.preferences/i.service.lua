10282e7e0b9a845cc7e55fe5e89a264e13552b11
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-07-13 23:06:22 +0200

    feat (Pavilion.Warcraft.Addons.PfuiZen.Persistence.Services.AddonSettings.UserPreferences.IService): introduce interfaces for user-preferences-service

---
+++ 30.pavilion.warcraft.addons.pfuizen/05.persistence/zen.persistence.contracts/services/addon.settings/user.preferences/i.service.lua
@@ -0,0 +1,9 @@
+﻿--[[@formatter:off]] local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"]); local Scopify = using "System.Scopify"; local EScopes = using "System.EScopes"; Scopify(EScopes.Function, {})
+
+local IServiceQueryable = using "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Contracts.Services.AddonSettings.UserPreferences.IServiceQueryable"
+local IServiceWriteable = using "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Contracts.Services.AddonSettings.UserPreferences.IServiceWriteable"
+
+local Class = using "[declare] [interface] [blend]" "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Contracts.Services.AddonSettings.UserPreferences.IService" { --@formatter:on
+    "IServiceQueryable", IServiceQueryable,
+    "IServiceWriteable", IServiceWriteable,
+}
---
+++ 30.pavilion.warcraft.addons.pfuizen/05.persistence/zen.persistence.contracts/services/addon.settings/user.preferences/i.service.writeable.lua
@@ -0,0 +1,11 @@
+﻿--[[@formatter:off]] local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"]); local Scopify = using "System.Scopify"; local EScopes = using "System.EScopes"; Scopify(EScopes.Function, {})
+
+local Class = using "[declare] [interface]" "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Contracts.Services.AddonSettings.UserPreferences.IServiceWriteable" -- @formatter:on
+
+--- @return boolean
+function Class:GreeniesGrouplootingAutomation_UpdateMode(value)
+end
+
+--- @return boolean
+function Class:GreeniesGrouplootingAutomation_UpdateActOnKeybind(value)
+end
