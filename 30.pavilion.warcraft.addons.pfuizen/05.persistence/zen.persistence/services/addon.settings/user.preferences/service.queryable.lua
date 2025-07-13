9abc4b3852993f932aac6eaa9d4ba1c5c43194be
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-07-13 23:51:40 +0200

    feat (Pavilion.Warcraft.Addons.PfuiZen.Persistence.Services.AddonSettings.UserPreferences): introduce IQueryableService and IWriteableService interfaces in queryable and writeable services

--- 30.pavilion.warcraft.addons.pfuizen/05.persistence/zen.persistence/services/addon.settings/user.preferences/service.queryable.lua
+++ 30.pavilion.warcraft.addons.pfuizen/05.persistence/zen.persistence/services/addon.settings/user.preferences/service.queryable.lua
@@ -1,13 +1,14 @@
 ﻿--[[@formatter:off]] local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"]); local Scopify = using "System.Scopify"; local EScopes = using "System.EScopes"; Scopify(EScopes.Function, {}) --[[@formatter:on]]
 
-
 local Nils = using "System.Nils"
 local Guard = using "System.Guard"
 local Fields = using "System.Classes.Fields"
 
 local UserPreferencesRepositoryQueryable = using "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Settings.UserPreferences.RepositoryQueryable"
 
-local Class = using "[declare]" "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Services.AddonSettings.UserPreferences.QueryableService"
+local Class = using "[declare] [blend]" "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Services.AddonSettings.UserPreferences.QueryableService" {
+    "IQueryableService", using "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Contracts.Services.AddonSettings.UserPreferences.IServiceQueryable"
+}
 
 
 Fields(function(upcomingInstance)
--- 30.pavilion.warcraft.addons.pfuizen/05.persistence/zen.persistence/services/addon.settings/user.preferences/service.writeable.lua
+++ 30.pavilion.warcraft.addons.pfuizen/05.persistence/zen.persistence/services/addon.settings/user.preferences/service.writeable.lua
@@ -1,13 +1,23 @@
 ﻿--[[@formatter:off]] local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"]); local Scopify = using "System.Scopify"; local EScopes = using "System.EScopes"; Scopify(EScopes.Function, {})
+
 local Guard   = using "System.Guard"
+local Fields  = using "System.Classes.Fields"
 
 local PfuiZenDbContext                            = using "Pavilion.Warcraft.Addons.PfuiZen.Persistence.EntityFramework.PfuiZen.DBContext"
 local UserPreferencesUnitOfWork                   = using "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Settings.UserPreferences.UnitOfWork"
 local SGreeniesGrouplootingAutomationMode         = using "Pavilion.Warcraft.Addons.PfuiZen.Foundation.Contracts.Strenums.SGreeniesGrouplootingAutomationMode"
 local SGreeniesGrouplootingAutomationActOnKeybind = using "Pavilion.Warcraft.Addons.PfuiZen.Foundation.Contracts.Strenums.SGreeniesGrouplootingAutomationActOnKeybind"
 
-local Class = using "[declare]" "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Services.AddonSettings.UserPreferences.WriteableService" -- @formatter:on
+local Class = using "[declare] [blend]" "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Services.AddonSettings.UserPreferences.WriteableService" { -- @formatter:on
+    "IWriteableService", using "Pavilion.Warcraft.Addons.PfuiZen.Persistence.Contracts.Services.AddonSettings.UserPreferences.IServiceWriteable"
+}
 
+
+Fields(function(upcomingInstance)
+    upcomingInstance._userPreferencesUnitOfWork = nil
+
+    return upcomingInstance
+end)
 
 function Class:New(userPreferencesUnitOfWork)
     Scopify(EScopes.Function, self)
