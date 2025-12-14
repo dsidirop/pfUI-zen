f6082992fa65e32637bc1ff93542e787df69ea36
Kyriakos Sidiropoulos <dominick.sidiropoulos@gmail.com>
2025-12-14 17:35:15 +0100

    clean (autoloot.engine.mediator.service.handle.ggaanaokc.partial.lua): trivial tweaks

--- 30_pfzautoloot/20_mediators/mediators/for.autoloot.engine/autoloot.engine.mediator.service.handle.ggaanaokc.partial.lua
+++ 30_pfzautoloot/20_mediators/mediators/for.autoloot.engine/autoloot.engine.mediator.service.handle.ggaanaokc.partial.lua
@@ -1,6 +1,6 @@
 ﻿--[[@formatter:off]] local using = assert((_G or getfenv(0) or {})["ZENSHARP:USING"]); local Scopify = using "System.Scopify"; local EScopes = using "System.EScopes"; Scopify(EScopes.Function, {})
 
-local Guard  = using "System.Guard" 
+local Guard   = using "System.Guard"
 
 local GreeniesGrouplootingAutomationApplyNewModeCommand = using "Pavilion.Warcraft.Addons.PfuiZen.Autoloot.Controllers.Contracts.Commands.GreeniesGrouplootingAutomation.ApplyNewModeCommand"
 
@@ -10,12 +10,12 @@ function Class:Handle_GreeniesGrouplootingAutomationApplyNewModeCommand(command)
     Scopify(EScopes.Function, self)
 
     Guard.Assert.IsInstanceOf(command, GreeniesGrouplootingAutomationApplyNewModeCommand, "command")
+    
+    _autolootEngine:GreeniesGrouplootingAutomation_SwitchMode(command:GetNewValue()) -- order   todo   in here we should intercept even the exceptions and marshal them into a verdict-domain-event (indicating success/failure) which is how handlers are supposed to work in this respect!
 
-    _autolootEngine:GreeniesGrouplootingAutomation_SwitchMode(command:GetNewValue()) -- order   todo   suppress exceptions here   handlers advertise successes/failures through domain-events only!
-
-    local success = _userPreferencesService:GreeniesGrouplootingAutomation_UpdateMode(command:GetNewValue()) -- order
-    if success then
-        -- todo   raise success/failure domain-events here
+    local wasDifferentFromExistingValue = _userPreferencesService:GreeniesGrouplootingAutomation_UpdateMode(command:GetNewValue()) -- order
+    if not wasDifferentFromExistingValue then
+        -- todo  use logging here    ("[AEMS.HGGAANMC.900] Info: The new value '%s' for GreeniesGrouplootingAutomation mode is the same as the existing one; no update was necessary.", command:GetNewValue())
     end
 
     return self
